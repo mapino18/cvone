2497ccc87e994cb67cad4d0e47f7122713c0654c
glr <mapinoweb@gmail.com>
2022-11-27 19:33:35 +0100

    init

---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Gérald LEGER</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex justify-content-center align-items-center header-transparent">
+
+    <nav id="navbar" class="navbar">
+      <ul>
+        <li><a class="nav-link scrollto active" href="#hero">Accueil</a></li>
+        <li><a class="nav-link scrollto" href="#about">A propos</a></li>
+        <li><a class="nav-link scrollto" href="#resume">Mon cv</a></li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav><!-- .navbar -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <h1>Gérald LEGER</h1>
+      <h2>En formation de Développeur Web et Web Mobile</h2>
+      <a href="#about" class="btn-scroll scrollto" title="Scroll Down"><i class="bx bx-chevron-down"></i></a>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Me Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <span>A propos</span>
+          <h2>A propos</h2>
+        </div>
+
+        <div class="row">
+          <div class="image col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
+          <div class="col-lg-8 d-flex flex-column align-items-stretch">
+            <div class="content ps-lg-4 d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul>
+                    <li><i class="bi bi-chevron-right"></i> <strong>Nom:</strong> <span>Gérald LEGER</span></li>
+                    <li><i class="bi bi-chevron-right"></i> <strong>Téléphone:</strong> <span>+33 06 16 74 05 59</span></li>
+                    <li><i class="bi bi-chevron-right"></i> <strong>Adresse:</strong> <span>11 Allée Pierre Méchain 18570 LA chapelle Saint Ursin</span></li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul>
+                    <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>45</span></li>
+                    <li><i class="bi bi-chevron-right"></i> <strong>Mail:</strong> <span>mapinoweb@gmail.com</span></li>
+                    <li><i class="bi bi-chevron-right"></i> <strong>Transport:</strong> <span>Permis B et véhicule</span></li>
+                  </ul>
+                </div>
+              </div>              
+            </div><!-- End .content-->
+
+            <div class="skills-content ps-lg-4">
+              <div class="progress">
+                <span class="skill">HTML <i class="val">90%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+
+              <div class="progress">
+                <span class="skill">CSS <i class="val">90%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+
+              <div class="progress">
+                <span class="skill">JavaScript <i class="val">50%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+
+              <div class="progress">
+                <span class="skill">Symphony 6 <i class="val">50%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+
+              <div class="progress">
+                <span class="skill">Python<i class="val">50%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- fin de section A propos -->
+
+    <!-- ======= Section du CV ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <span>Mon CV</span>
+          <h2>Mon CV</h2>
+          <p>Désireux de relever de nouveaux challenges,
+            motiver pour apprendre, également rigoureux,
+            respectueux ,ponctuel, autonome et curieux, je
+            souhaiterais rejoindre vos effectifs afin de participer
+            aux développement de vos projets.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <h3 class="resume-title">Formations</h3>
+            <div class="resume-item">
+              <h4>Titre professionnel Développeur Web et Web Mobile ( en cours )</h4>
+              <h5>2022 - 2023</h5>
+              <p><em>Chambre de commerce et d'inndustrie, Bourges (18)</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>BTS Services informatiques aux Organisations</h4>
+              <h5>2012 - 2015</h5>
+              <p><em>CNED, Poitiers (86) </em></p>
+              <p>Solution Logicielles et Application Métiers</p>
+            </div>
+            <div class="resume-item">
+              <h4>Bac STI</h4>
+              <h5>2012 - 2015</h5>
+              <p><em>Lycée de Presle; Cusset (03)</em></p>
+              <p>Bois et Matériaux Associés</p>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <h3 class="resume-title">Expériences Professionnelles</h3>
+            <div class="resume-item">
+              <h4>Technicien métreur</h4>
+              <h5>01/2022 - 07/2022</h5>
+              <p><em>AEC Habitat - Bourges (18)</em></p>
+              <p>
+                <ul>
+                  <li>Réalisation de métré et mise en fabrication</li>
+                </ul>
+              </p>
+            </div>
+            <div class="resume-item">
+              <h4>Technicien métreur</h4>
+              <h5>11/2015 - 01/2022</h5>
+              <p><em>Sevestre Menuiseries</em></p>
+              <p>
+                <ul>
+                  <li>Réalisation de tableaux de bord via "POWER BI".</li>
+                  <li>Gestion du logiciel métier "PRODEVIS"</li>
+                  <li>Réalisation de devis</li>
+                  <li>Réalisation de plan technique via "AUTOCAD"</li>
+                </ul>
+              </p>
+            </div>
+            <div class="resume-item">
+              <h4>Technicien SI</h4>
+              <h5>09/2015 - 10/2015</h5>
+              <p><em>EDF Centrale Nucléaire de Belleville Sur Loire (18)</em></p>
+              <p>
+                <ul>
+                  <li>Extration de données </li>
+                  <li>Traitement des données par macro excel</li>
+                  <li>Création et modification de profil informatique</li>
+                </ul>
+              </p>
+            </div>
+            <div class="resume-item">
+              <h4>Stage développeur Web</h4>
+              <h5>02/2014 - 03/2015</h5>
+              <p><em>ID Clair, Bourges (18)</em></p>
+              <p>
+                <ul>
+                  <li>Création site vitrine de vapotage</li>
+                  <li>Création site vitrine de plombier</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End My Resume Section -->
+
+   
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">      
+      <div class="social-links">
+        <a href="https://www.facebook.com/gerald.leger18" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.linkedin.com/in/g%C3%A9rald-leger-537b8021/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        
+      </div>
+
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
